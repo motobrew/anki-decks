4862a2e6b591ff16be77450f6694cb6074a3252b
Fukao Moto <fukao@nagamina.com>
2023-07-02 09:27:38 +0900

    update

--- chatgpt-ts/src/index.ts
+++ chatgpt-ts/src/index.ts
@@ -12,11 +12,12 @@ export async function ask(question: string, model = "gpt-3.5-turbo-0613") {
   const content = `
   Hi, I'm an English learner.
   Give me 2 sentences using "${question}" as idiom, based on most commonly use cases.
-  But if the keyword I give you has some different meanings, make sentences as different meaing as possible. Shorter is better for each sentence. (8~12 words)
+  But if the keyword I give you has some different meanings, make sentences as different meaing as possible.
+  Shorter is better for each sentence. (8~12 words)
   And then, put them into 2 sections: English sentences and Japanese translation,
   In addition to that, enclose in <u> tags both the keyword and corresponding translation.
+
   Your response must be complied to the following format.
-  
   Sentences are just example if the keyword was "pop up".
 
   ## Response

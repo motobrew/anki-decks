7b6da1f58722adaa4aa060ee5e3c0685017e6d63
Fukao Moto <fukao@nagamina.com>
2023-07-25 06:31:49 +0900

    update

--- chatgpt-ts/src/index.ts
+++ chatgpt-ts/src/index.ts
@@ -11,18 +11,18 @@ const gptModel = "gpt-3.5-turbo-0613";
 
 export async function ask(question: string, model = gptModel) {
   const content = `
-## Instruction
+# Instruction
 Make two sentences using the words and idioms I am going to tell you.
 These sentences should correspond to the situations in which they are commonly used.
 Also, if a word or idiom has multiple meanings, please create sentences so that the meanings of the words are different.
 Each English sentence should be about 6-10 words.
 In addition to that, enclose in <u> tags both the keyword and corresponding translation.
-Your response must be complied to the following format.
+Your response must be complied to the following "Result Format".
 
-## word or idiom
-The phrase is "${question}"
+# word, phrase, idiom or sentence structure
+"${question}"
 
-## Result format (sample word is "pop-up")
+# Result Format ("pop-up" is as sample)
 <h3>English Sentences</h3>
 E-1: A <u>pop-up</u> alerted me of a new message on my phone.
 E-2: Unexpected obstacles often <u>pop up</u> at the worst times.
